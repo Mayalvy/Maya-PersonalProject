a386ab3ace1c6c1a0b40b9cbe9e29dd71c4d8a59
Mayalvy <mayalevy20@gmail.com>
2024-11-07 01:16:55 +0200

    update

--- Maya - Multi-User Todo Application/src/server.ts
+++ Maya - Multi-User Todo Application/src/server.ts
@@ -1,1 +1,29 @@
-console.log("hello world")
+import express from 'express'
+const app = express()
+const port = 3000;
+import cookieParser from 'cookie-parser';
+
+
+app.use(express.static('public'))
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
+
+  app.use(express.json());
+  app.use(cookieParser());
+
+
+
+  app.get('/', (req:any, res:any) => {
+    res.send('Hello World!')
+  })
+
+  //connection to db
+  const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://mayalevy20:e0ok6g2sVajOLOhu@cluster0.emeus.mongodb.net/Todo-App').then(()=>{
+  console.log('connected to db')
+})
+.catch((err:any)=>{
+  console.log(err)
+});
